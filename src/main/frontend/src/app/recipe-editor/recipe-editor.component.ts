3623b41091cbba0402126e0e310a7b96f2f2e1d1
Roeland H.P. Kegel <r.h.p.kegel@gmail.com>
2023-06-11 16:11:18 +0200

    save & load local

--- src/main/frontend/src/app/recipe-editor/recipe-editor.component.ts
+++ src/main/frontend/src/app/recipe-editor/recipe-editor.component.ts
@@ -1,34 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {exampleRecipe, Recipe} from "../recipe-page/recipe.model";
 import {MatDialog} from "@angular/material/dialog";
+import {SafeUrl} from '@angular/platform-browser';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-recipe-editor',
   templateUrl: './recipe-editor.component.html',
   styleUrls: ['./recipe-editor.component.scss']
 })
-export class RecipeEditorComponent{
-  public recipe: Recipe = this.initRecipe();
+export class RecipeEditorComponent {
+  public recipe: Recipe = exampleRecipe;
   public recipeWidth = 30;
   doublePage: boolean = false;
-  constructor(public dialog: MatDialog) { }
+  selectedFiles: File[] | undefined;
 
-  zoomInRecipe(){
+  constructor() {
+  }
+
+  zoomInRecipe() {
     this.recipeWidth += 10;
   }
-  zoomOutRecipe(){
+
+  zoomOutRecipe() {
     this.recipeWidth -= 10;
   }
 
-  printRecipe(){
-    this.recipeWidth=50;
-    this.doublePage=true;
-    setTimeout(()=>window.print());
+  printRecipe() {
+    this.recipeWidth = 50;
+    this.doublePage = true;
+    setTimeout(() => window.print());
   }
 
   doublePageToggle() {
     this.doublePage = !this.doublePage;
-    if (this.doublePage && this.recipeWidth > 50){
+    if (this.doublePage && this.recipeWidth > 50) {
       this.recipeWidth = 50;
     }
   }
@@ -39,5 +45,27 @@ export class RecipeEditorComponent{
       steps: [],
       title: ''
     }
+  }
+
+  downloadURI() {
+    const blob = new Blob([JSON.stringify(this.recipe)], {type: 'application/json'});
+    const uri = window.URL.createObjectURL(blob);
+    //window.location.assign(uri);
+
+    var a = document.createElement('a');
+    a.href = uri;
+    a.download = this.recipe.title ? this.recipe.title : 'untitled recipe'
+    console.log(a.href, a.download)
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
+
+  updateCurrentRecipe(e: any) {
+    let reader = new FileReader();
+    reader.onload = () => {
+      this.recipe = JSON.parse(reader.result as string);
+    };
+    reader.readAsText(e.target.files[0], 'utf-8');
   }
 }
