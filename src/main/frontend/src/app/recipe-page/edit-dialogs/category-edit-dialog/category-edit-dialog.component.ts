3dfd9bc5bdfca8197a827cf80851afa0fabbf2a8
Roeland H.P. Kegel <r.h.p.kegel@gmail.com>
2023-10-06 11:04:14 +0200

    filter autocompletes on tags and categories

--- src/main/frontend/src/app/recipe-page/edit-dialogs/category-edit-dialog/category-edit-dialog.component.ts
+++ src/main/frontend/src/app/recipe-page/edit-dialogs/category-edit-dialog/category-edit-dialog.component.ts
@@ -1,28 +1,43 @@
-import {AfterViewInit, Component, Inject} from '@angular/core';
-import {FormBuilder, FormControl} from "@angular/forms";
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {RecipeService} from "../../../recipe.service";
 import {BaseEditDialogDirective} from "../base-edit-dialog.directive";
+import {map, Observable, startWith, tap} from "rxjs";
 
 @Component({
-    selector: 'app-category-edit-dialog',
-    templateUrl: './category-edit-dialog.component.html',
-    styleUrls: ['./category-edit-dialog.component.scss']
+  selector: 'app-category-edit-dialog',
+  templateUrl: './category-edit-dialog.component.html',
+  styleUrls: ['./category-edit-dialog.component.scss']
 })
-export class CategoryEditDialogComponent extends BaseEditDialogDirective<CategoryEditDialogComponent, string> implements AfterViewInit{
-    public form = new FormControl<string>(this.data);
-    public categoryOptions: string[] = this.knownCategories;
+export class CategoryEditDialogComponent extends BaseEditDialogDirective<CategoryEditDialogComponent, string> implements OnInit{
+  public form = new FormControl<string>(this.data);
+  public categoryOptions: string[] = this.knownCategories;
+  public filteredCategoryOptions: Observable<string[]> | undefined;
 
-    constructor(public dialogRef: MatDialogRef<CategoryEditDialogComponent>,
-                @Inject(MAT_DIALOG_DATA) public data: string,
-                public recipeService: RecipeService) {
-        super(dialogRef, data)
-    }
+  constructor(public dialogRef: MatDialogRef<CategoryEditDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: string,
+              public recipeService: RecipeService) {
+    super(dialogRef, data)
+  }
 
-    private get knownCategories(): string[] {
-        let result: string[] = [];
-        this.recipeService.getRecipes().forEach(r => result = result.concat(r.category ? r.category : []))
-        result = result.sort((a,b)=> a.localeCompare(b));
-        return result;
-    }
+  private filterCategoryOptions(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.categoryOptions.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  private get knownCategories(): string[] {
+    let result: string[] = [];
+    this.recipeService.getRecipes().forEach(r => result = result.concat(r.category ? r.category : []))
+    result = [...new Set(result)].sort((a, b) => a.localeCompare(b));
+    return result;
+  }
+
+  ngOnInit(): void {
+    this.filteredCategoryOptions = this.form.valueChanges.pipe(
+      startWith(''),
+      map(value => this.filterCategoryOptions(value || '')),
+    );
+  }
 }
--- src/main/frontend/src/app/recipe-page/edit-dialogs/tag-edit-dialog/tag-edit-dialog.component.ts
+++ src/main/frontend/src/app/recipe-page/edit-dialogs/tag-edit-dialog/tag-edit-dialog.component.ts
@@ -4,6 +4,7 @@ import {FormBuilder, FormControl} from "@angular/forms";
 import {BaseEditDialogDirective} from "../base-edit-dialog.directive";
 import {RecipeService} from "../../../recipe.service";
 import {MatAutocompleteTrigger} from "@angular/material/autocomplete";
+import {map, Observable, startWith} from "rxjs";
 
 @Component({
   selector: 'app-tag-edit-dialog',
@@ -13,6 +14,8 @@ import {MatAutocompleteTrigger} from "@angular/material/autocomplete";
 export class TagEditDialogComponent extends BaseEditDialogDirective<TagEditDialogComponent, string[]>{
   public form = this.fb.array(this.data);
   public tagOptions: string[] = this.knownTags;
+  public filteredTagOptions: Observable<string[]> | undefined;
+  public focusedFormControl: FormControl<string | null> | undefined;
 
   constructor(private fb: FormBuilder, public dialogRef: MatDialogRef<TagEditDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: string[], public recipeService: RecipeService) {
     super(dialogRef, data)
@@ -22,10 +25,28 @@ export class TagEditDialogComponent extends BaseEditDialogDirective<TagEditDialo
     this.form.push(new FormControl(''))
   }
 
+  ngOnInit(): void {
+    this.filteredTagOptions = this.form.valueChanges.pipe(
+      startWith(''),
+      map(() => this.filterTagOptions(this.focusedFormControl?.value || '')),
+    );
+  }
+
+  private filterTagOptions(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.tagOptions.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
   private get knownTags(): string[] {
     let result: string[] = [];
     this.recipeService.getRecipes().forEach(r => result = result.concat(r.tags ? r.tags : []))
     result = [...new Set(result)].sort((a,b)=> a.localeCompare(b));
     return result;
+  }
+
+  onTagFocus(tag: FormControl<string | null>) {
+    this.focusedFormControl = tag;
+    this.focusedFormControl.updateValueAndValidity({onlySelf: false, emitEvent: true});
   }
 }
